828624fa3bc0e31177f76f3a0df208f6173de18c
Nikolaus Demmel <demmeln@in.tum.de>
2018-05-17 19:00:32 +0200

    tweaks

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile_ubuntu-14.04 -t ubuntu-dev-base:14.04 .
-docker build -f Dockerfile_ubuntu-16.04 -t ubuntu-dev-base:16.04 .
+docker build --pull -f Dockerfile_ubuntu-14.04 -t ubuntu-dev-base:14.04 .
+docker build --pull -f Dockerfile_ubuntu-16.04 -t ubuntu-dev-base:16.04 .
 
 docker tag ubuntu-dev-base:14.04 nikolausdemmel/ubuntu-dev-base:14.04
 docker tag ubuntu-dev-base:16.04 nikolausdemmel/ubuntu-dev-base:16.04
