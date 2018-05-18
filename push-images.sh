8c01908fee2279731d0b745f3713790951bf7328
Nikolaus Demmel <demmeln@in.tum.de>
2018-05-18 14:50:34 +0200

    ci

--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-docker login
 
-docker push nikolausdemmel/ubuntu-dev-base:14.04
+# try to push and login if it fails
+if docker push nikolausdemmel/ubuntu-dev-base:14.04 2>&1 | grep 'authentication required' > /dev/null; then
+    docker login
+    docker push nikolausdemmel/ubuntu-dev-base:14.04
+fi
+
 docker push nikolausdemmel/ubuntu-dev-base:16.04
 
